ffb21b30abcd5b13650a0d0f125259ed4be0a78d
MagicWinnie <magicwinnie@yandex.ru>
2019-11-03 21:43:36 +0700

    Syntax

---
+++ Lesson_1/four.py
@@ -0,0 +1,3 @@
+a = input("a=")
+b = input("b=")
+print("a={0};b={1}".format(b,a))
---
+++ Lesson_1/second.py
@@ -0,0 +1,4 @@
+name = input()
+surname = input()
+age = int(input())
+print("Вас зовут {0} {1}, Вам {2} лет.".format(name, surname, age))
